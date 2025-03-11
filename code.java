34fbf704c31ee27f73955c530d57a42ce543bafe
shivu0988766 <patilshivaprasad05@gmail.com>
2025-03-11 13:45:38 +0530

    new code

---
+++ code.java
@@ -0,0 +1,37 @@
+class Calculator {
+    // Method for addition
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    // Method for subtraction
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    // Method for multiplication
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    // Method for division
+    public double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero");
+            return 0;
+        }
+        return (double) a / b;
+    }
+   
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int a = 10, b = 5; // Predefined values
+
+        System.out.println("Addition: " + calc.add(a, b));
+        System.out.println("Subtraction: " + calc.subtract(a, b));
+        System.out.println("Multiplication: " + calc.multiply(a, b));
+        System.out.println("Division: " + calc.divide(a, b));
+	
+    }
+}
